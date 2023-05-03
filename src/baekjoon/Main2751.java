716d650db727c20153ffcfe5cfe3352402bbf988
znhee <njh2953@gmail.com>
2023-05-03 16:39:25 +0900

    백준 2839

---
+++ src/baekjoon/Main2751.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Main2751 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
