529b626c427c721fe6b2cc21bdde6eec559433f9
znhee <njh2953@gmail.com>
2023-05-22 23:35:49 +0900

    백준 11047

---
+++ src/greedy/Main11047.java
@@ -0,0 +1,42 @@
+package greedy;
+
+/**
+ 문제
+	준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+	
+	동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+
+ 입력
+	첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+
+	둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
+
+ 출력
+	첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력한다.
+ */
+
+import java.util.*;
+
+public class Main11047 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		// 그리디 알고리즘 ->  최대한 큰 동전 먼저 쓰기
+		int count = 0;
+		for(int i=N-1; i>=0; i--) {
+			if(arr[i] <= K) {
+				count += (K/arr[i]);
+				K %= arr[i];
+			}
+		}
+		System.out.print(count);
+	}
+
+}
