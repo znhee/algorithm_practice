4f45d21eeeaed4d749e81b9b56dbfd71de239d37
znhee <njh2953@gmail.com>
2023-06-09 22:57:31 +0900

    프로그래머스 추억점수

---
+++ src/map/Main_추억점수.java
@@ -0,0 +1,27 @@
+package map;
+
+/**
+ 프로그래머스 1단계 : 추억 점수
+*/
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0; i<name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        int[] answer = new int[photo.length];
+        for(int i=0; i<photo.length; i++) {
+            int count = 0;
+            for(int j=0; j<photo[i].length; j++) {
+                if(map.containsKey(photo[i][j])) {
+                    count += map.get(photo[i][j]);
+                } 
+                answer[i] = count;
+            }
+        }
+        
+        return answer;
+    }
+}
